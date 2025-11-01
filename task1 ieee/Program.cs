098c39225a633f782430f70820c1de95c3f6f21a
Zahraa <zahraa.yasser.2006@gmail.com>
2025-11-01 15:09:40 +0200

    Add project files.

---
+++ task1 ieee/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_ieee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number : ");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter the second number : ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float sum = num1 + num2;
+            Console.WriteLine("the sum is : " + sum);
+
+        }
+    }
+}
